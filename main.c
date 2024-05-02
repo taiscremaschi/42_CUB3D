6a3d4005097be0b7b73bffc9207a75660b732eff
taiscremaschi <taiscremaschi@gmail.com>
2024-05-02 14:41:57 +0100

    issue gestao das janelas check

--- main.c
+++ main.c
@@ -33,19 +33,16 @@ int	main(int ac, char **av)
 {
 	t_main	main;
 
-	main.player.x = 0;
-	main.player.y = 0;
-	main.moves_counter = 0;
 	if (ac != 2)
-		exit(write(1, "Error\n", 6));
+		exit(write(2, "Error\n", 6));
 	check_map(av, &main);
 	main.mlx = mlx_init();
-	main.win = mlx_new_window(main.mlx, ((main.larg) * 64), main.altura
-			* 64, "cub3D");
+	main.win = mlx_new_window(main.mlx, ((main.larg) * 100), main.altura
+			* 200, "cub3D"); //janela grande 
 	image_inicialize(&main);
 	mlx_loop_hook(main.mlx, render_image, &main);
-	mlx_hook(main.win, 2, 1L << 0, read_esc, &main);
-	mlx_hook(main.win, 33, 1L << 2, end, &main);
+	mlx_hook(main.win, 2, 1L << 0, read_esc, &main); //fechar com esc
+	mlx_hook(main.win, 33, 1L << 2, end, &main); // fechar com o x
 	mlx_loop(main.mlx);
 	return (0);
 }
