b06c3b7beac1640c8fca9883fadb558e62ccb760
taiscremaschi <taiscremaschi@gmail.com>
2024-05-03 20:13:19 +0100

    funcao pra guardar ok

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,101 +6,77 @@
 /*   By: tbolzan- <tbolzan-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 10:48:01 by tbolzan-          #+#    #+#             */
-/*   Updated: 2024/05/03 14:21:25 by tbolzan-         ###   ########.fr       */
+/*   Updated: 2024/05/03 20:12:34 by tbolzan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub.h"
 
-int search_and_save_args(t_main *main)
+int	save_textures(int *j, char *line, char **filename, int size)
 {
-    int i = 0;
-    int j;
-    char *line;
-    int flag = 0;
-    int check_paths = 0; 
-    while(main->file_content[i]!= NULL)
+	int	flag;
+	flag = 0;
+	*j += size;
+	while (line[*j] == ' ' || line[*j] == '\t')
+		(*j)++;
+    if (line[*j] == '\n')
     {
-        line = ft_substr(main->file_content[i], 0, ft_strlen(main->file_content[i]));
-        
-        j = 0;
-        while (line[j] != '\0' && line[j] != '\n')
-        {
-            while ((line[j] != '\0' && line[j] == ' ' )|| line[j] == '\t')
-                j++;
-            if(ft_strncmp(&line[j], "NO ", 3) == 0)
-            {
-                j += 4;
-                flag = j;
-                while(line[flag] != ' ' || line[flag] != '\t' || line[flag] != '\n' || line[flag] != '\0')
-                    flag++;
-                main->textures.north = ft_substr(line, (j), (flag - j));
-                j = flag;
-                check_paths++;
-            }
-            if(ft_strncmp(&line[j], "SO ", 3) == 0)
-            {
-                j += 4;
-                flag = j;
-                while(line[flag] != ' ' || line[flag] != '\t' || line[flag] != '\n' || line[flag] != '\0')
-                    flag++;
-                main->textures.south = ft_substr(line, (j), (flag - j));
-                j = flag;
-                check_paths++;
-
-            }
-            if(ft_strncmp(&line[j], "WE ", 3) == 0)
-            {
-                j += 4;
-                flag = j;
-                while(line[flag] != ' ' || line[flag] != '\t' || line[flag] != '\n' || line[flag] != '\0')
-                    flag++;
-                main->textures.west= ft_substr(line, (j), (flag - j));
-                j = flag;
-                check_paths++;
-
-            }
-            if(ft_strncmp(&line[j], "EA ", 3) == 0)
-            {
-                j += 4;
-                flag = j;
-                while(line[flag] != ' ' || line[flag] != '\t' || line[flag] != '\n' || line[flag] != '\0')
-                    flag++;
-                main->textures.east = ft_substr(line, (j), (flag - j));
-                j = flag; 
-                check_paths++;
-
-            }
-            if(ft_strncmp(&line[j], "F ", 2) == 0)
-            {
-                j += 3;
-                flag = j;
-                while(line[flag] != ' ' || line[flag] != '\t' || line[flag] != '\n' || line[flag] != '\0')
-                    flag++;
-                main->textures.floor_color = ft_substr(line, (j), (flag - j));
-                j = flag;   
-                check_paths++;
-
-            }
-            if(ft_strncmp(&line[j], "C ", 2) == 0)
-            {
-                j += 3;
-                flag = j;
-                while(line[flag] != ' ' || line[flag] != '\t' || line[flag] != '\n' || line[flag] != '\0')
-                    flag++;
-                main->textures.ceiling_color = ft_substr(line, (j), (flag - j));
-                j = flag;
-                check_paths++;
-            }
-            j++;
-        }
-        if(line)
-            free (line);
-        i++;
+        return (-1);
     }
-    if(check_paths != 6)
-		exit(ft_putstr_fd("Error in args of file\n", 2));
-    return 0;
+	flag = *j;
+	while (line[flag] != '\0' && line[flag] != ' ' && line[flag] != '\t'
+		&& line[flag] != '\n')
+		flag++;
+	*filename = ft_substr(line, (*j), (flag - *j));
+	*j = flag;
+	return (1);
+}
+
+int	compare_args(char *line, int *j, t_main *main)
+{
+	int	count;
+    
+	count = 0;
+	if (ft_strncmp(&line[*j], "NO ", 3) == 0)
+		count += save_textures(j, line, &main->textures.north, 3);
+	else if (ft_strncmp(&line[*j], "SO ", 3) == 0)
+		count += save_textures(j, line, &main->textures.south, 3);
+	else if (ft_strncmp(&line[*j], "WE ", 3) == 0)
+		count += save_textures(j, line, &main->textures.west, 3);
+	else if (ft_strncmp(&line[*j], "EA ", 3) == 0)
+		count += save_textures(j, line, &main->textures.east, 3);
+	else if (ft_strncmp(&line[*j], "F ", 2) == 0)
+		count += save_textures(j, line, &main->textures.floor_color, 2);
+	else if (ft_strncmp(&line[*j], "C ", 2) == 0)
+		count += save_textures(j, line, &main->textures.ceiling_color, 2);
+	return (count);
+}
+
+int	search_and_save_args(t_main *main)
+{
+	int		i;
+	int		j;
+	char	*line;
+	int		count;
+
+	i = 0;
+	count = 0;
+	while (main->file_content[i] != NULL)
+	{
+		line = ft_strdup(main->file_content[i]);
+		j = 0;
+		while (line[j] != '\0' && line[j] != '\n')
+		{
+			while ((line[j] != '\0' && line[j] == ' ') || line[j] == '\t')
+				j++;
+			count += compare_args(line, &j, main);
+			j++;
+		}
+		if (line)
+			free(line);
+		i++;
+	}
+	return (count);
 }
 
 char	**save_file(char **map, int fd)
@@ -122,30 +98,51 @@ char	**save_file(char **map, int fd)
 	return (map);
 }
 
-void check_arg_and_fd(char **av, int fd)
+void	check_arg_and_fd(char **av, int fd)
 {
 	if (ft_strchr(av[1], '.') == NULL || ft_strncmp(ft_strchr(av[1], '.'),
 			".cub\0", 5) != 0)
 		exit(write(2, "Error, wrong extension\n", 23));
 	if (fd <= 0)
 		exit(ft_putstr_fd("Error in opening FD\n", 2));
+}
+
+void inicialize_txt(t_main *main)
+{
+    main->textures.ceiling_color = NULL;
+    main->textures.east = NULL;
+    main->textures.floor_color = NULL;
+    main->textures.north  = NULL;
+    main->textures.south  = NULL;
+    main->textures.west  = NULL;
     
 }
 
 void	parsing_map(char **av, t_main *main)
 {
-	int	fd;
+	int fd;
+
 	fd = open(av[1], O_RDONLY);
-    check_arg_and_fd(av, fd);
+	check_arg_and_fd(av, fd);
 	main->file_content = save_file(NULL, fd);
-    //verificar os paths se estao corretos
-    search_and_save_args(main);
-    //separar o mapa dos paths 
-    //verificar se o mapa esta correto com os caracteres depois as paredes 
-    // com uma especie de flood fill 
-	map_rectangle(main); //esse n preciso
+    inicialize_txt(main);
+	// verificar os paths se estao corretos
+	if (search_and_save_args(main) != 6)
+		exit(ft_putstr_fd("Error in args of file\n", 2));
+    printf("norte %s\n", main->textures.north);
+    printf("sul %s\n", main->textures.south);
+    printf("west %s\n", main->textures.west);
+    printf(" east %s\n", main->textures.east);
+    printf("floor %s\n", main->textures.floor_color);
+    printf("celing %s\n", main->textures.ceiling_color);
+
+
+	// separar o mapa dos paths
+	// verificar se o mapa esta correto com os caracteres depois as paredes
+	// com uma especie de flood fill
+	map_rectangle(main); // esse n preciso
 	map_validate(main);
 	map_caracteres(main);
 	map_paredes(main);
-	//check_map_alg(main, copy_map(main));
+	// check_map_alg(main, copy_map(main));
 }
