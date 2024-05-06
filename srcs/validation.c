7e3db5aabfa7a63274e8c0b946ee591847be67b8
taiscremaschi <taiscremaschi@gmail.com>
2024-05-06 13:52:48 +0100

    funcao check and save_player

--- srcs/validation.c
+++ srcs/validation.c
@@ -6,7 +6,7 @@
 /*   By: tbolzan- <tbolzan-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 15:07:11 by paula             #+#    #+#             */
-/*   Updated: 2024/05/06 13:37:42 by tbolzan-         ###   ########.fr       */
+/*   Updated: 2024/05/06 13:50:48 by tbolzan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,10 @@ void	map_paredes(t_main *main)
 	largura = main->larg - 1;
 	while (main->file_content[0][++i] != '\n')
 	{
-		if ((main->file_content[0][i] != '1' && main->file_content[0][i] != '\n')
-			|| (main->file_content[altura][i] != '1' && main->file_content[altura][i] != '\n'))
+		if ((main->file_content[0][i] != '1'
+				&& main->file_content[0][i] != '\n')
+			|| (main->file_content[altura][i] != '1'
+				&& main->file_content[altura][i] != '\n'))
 		{
 			free_map(main->file_content);
 			exit(ft_putstr_fd("erro na parede de cima ou baixo\n", 1));
@@ -43,7 +45,8 @@ void	map_paredes(t_main *main)
 	i = -1;
 	while (++i < altura)
 	{
-		if (main->file_content[i][0] != '1' && main->file_content[i][largura] != '1')
+		if (main->file_content[i][0] != '1'
+			&& main->file_content[i][largura] != '1')
 		{
 			free_map(main->file_content);
 			exit(ft_putstr_fd("erro na parede dos lados\n", 1));
@@ -72,59 +75,44 @@ void	map_rectangle(t_main *main)
 	main->altura = alt;
 }
 
-// void	check_letters(t_main *main, int i, int j, int *true_p)
-// {
-// 	if (main->file_content[i][j] == 'P')
-// 	{
-// 		(*true_p)++;
-// 		main->player.x = j;
-// 		main->player.y = i;
-// 	}
-// 	else if (main->file_content[i][j] == 'C')
-// 		main->col++;
-// }
-
-void	map_caracteres(t_main *main)
+void	check_and_save_player(t_main *main, char *letters)
 {
 	int	i;
 	int	j;
 	int	flag_player;
-	int x; 
-	char *letters;
+	int	x;
 
 	i = -1;
 	flag_player = 0;
-	main->col = 0;
-	letters = "NSEW";
-	
 	while (main->file_content[++i] != NULL)
 	{
 		j = -1;
 		while (main->file_content[i][++j] != '\0')
 		{
 			x = -1;
-			while(++x < 4)
+			while (++x < 4)
 			{
 				if (main->file_content[i][j] == letters[x] && flag_player == 0)
 				{
+					main->player.x = j;
+					main->player.y = i;
 					flag_player = 1;
 					break ;
 				}
-				else if (main->file_content[i][j] == letters[x] && flag_player == 1)
-					exit(ft_putstr_fd("Error in character\n", 2));
+				else if (main->file_content[i][j] == letters[x]
+					&& flag_player == 1)
+					exit(ft_putstr_fd("Error: more than one player\n", 2));
 			}
 		}
 	}
-			//check_letters(main, i, j, &true_p);	
 }
-
 
 int	validate_characteres(char *line_map)
 {
 	char	*valid_chars;
 	int		i;
 	int		j;
-		
+
 	i = 0;
 	j = 0;
 	valid_chars = "NSEW01\t\n ";
@@ -143,7 +131,6 @@ int	validate_characteres(char *line_map)
 	return (0);
 }
 
-
 void	check_chars(t_main *main)
 {
 	int	i;
@@ -161,14 +148,8 @@ void	check_chars(t_main *main)
 	}
 }
 
-
 void	validate_map(t_main *main)
 {
 	check_chars(main);
-	map_caracteres(main);
-
-
-	
-
-	
+	check_and_save_player(main, "NSEW");
 }
