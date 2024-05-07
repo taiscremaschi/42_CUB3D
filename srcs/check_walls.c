852f3bd26c1fbed2686c2da1150e7d3a46263959
taiscremaschi <taiscremaschi@gmail.com>
2024-05-07 13:35:39 +0100

    OH YES alg tata cheeck

--- srcs/check_walls.c
+++ srcs/check_walls.c
@@ -6,7 +6,7 @@
 /*   By: tbolzan- <tbolzan-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 15:06:52 by paula             #+#    #+#             */
-/*   Updated: 2024/05/07 13:11:50 by tbolzan-         ###   ########.fr       */
+/*   Updated: 2024/05/07 13:34:41 by tbolzan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,6 +95,7 @@ int check_zero(char**map_copy, int x, int y)
 
 bool check_x_left(char**map_copy, int x, int y)
 {
+	printf("entrou no left \n");
 	
 	while(x >= 0)
 	{
@@ -112,6 +113,7 @@ bool check_x_left(char**map_copy, int x, int y)
 
 bool check_x_right(char**map_copy, int x, int y)
 {
+	printf("entrou no right \n");
 	
 	while(map_copy[y][++x] != '\0')
 	{
@@ -128,8 +130,7 @@ bool check_x_right(char**map_copy, int x, int y)
 
 bool check_y_up(char**map_copy, int x, int y)
 {
-	
-	while(y >= 0)
+	while(y > 0)
 	{
 		y--;
 		if(map_copy[y][x] == '1')
@@ -144,10 +145,12 @@ bool check_y_up(char**map_copy, int x, int y)
 
 bool check_y_down(char**map_copy, int x, int y, int height)
 {
-	
-	while( y < height)
+
+
+
+	while( y < (height -1))
 	{
-		y--;
+		y++;
 		if(map_copy[y][x] == '1')
 			return true;
 		if( map_copy[y][x] == '\n' || map_copy[y][x] == ' ' || map_copy[y][x] == '\t')
@@ -161,30 +164,29 @@ bool check_y_down(char**map_copy, int x, int y, int height)
 
 bool alg_tata(char **map_copy, int x, int y, int height)
 {
-	int count  = check_zero(map_copy, x, y);
-	while(map_copy[y])
+	while(map_copy[y] != NULL)
 	{
 		x = 0;
 		while(map_copy[y][x] != '\0' || map_copy[y][x] != '\n')
 		{
-			if(map_copy[y][x] == '1' || map_copy[y][x] == ' ' || map_copy[y][x] == '\t')
+			if(map_copy[y][x] == '\0' || map_copy[y][x] == '\n')
+				break ;
+			else if(map_copy[y][x] == '1' || map_copy[y][x] == ' ' || map_copy[y][x] == '\t')
 				x++;
-			if(map_copy[y][x] == '0')
+			else if(map_copy[y][x] == '0')
 			{
 				if (!check_y_down(map_copy, x, y, height) || !check_y_up(map_copy, x, y))
 						return false;
 				if(!check_x_left(map_copy, x, y) || !check_x_right(map_copy, x, y))
 					return false;
-				count --;
+				x++;
 			}
 			else 
 				x++;
 		}
 		y++;
 	}
-	if(count == 0)
-		return true;
-	return false;	
+	return true;	
 }
 
 void	check_map_alg(t_main *main, char **copy_map_temp)
@@ -194,7 +196,7 @@ void	check_map_alg(t_main *main, char **copy_map_temp)
 		free_map(main->file_content);
 		exit(ft_putstr_fd("Error in copy map\n", 2));
 	}
-	if(!alg_tata(copy_map_temp, main->player.x, main->player.y, main->height))
+	if(alg_tata(copy_map_temp, main->player.x, main->player.y, main->height) == false)
 	{
 		//free_map(main->file_content);
 		//free_map(copy_map_temp);
