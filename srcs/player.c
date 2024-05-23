7e8bba665c7254f550f0def77b9c0c7308760333
Paula Biazotto <pds_paula@hotmail.com>
2024-05-23 10:36:31 +0100

    fix: protected player angle from double overflow

--- srcs/player.c
+++ srcs/player.c
@@ -6,7 +6,7 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 09:31:23 by paula             #+#    #+#             */
-/*   Updated: 2024/05/23 10:14:50 by paula            ###   ########.fr       */
+/*   Updated: 2024/05/23 10:35:54 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,16 @@ void	rotate2(double angle, t_vector *vector)
 	vector->dy = sin(angle);
 }
 
-// proteger o PI, nao pode passar de 360
+// protected angle from double overflow
 void	rotate_player(t_player *player, double dangle)
 {
 	double	perpendicular_angle;
 
 	player->angle += dangle;
+	if (player->angle < 0)
+		player->angle += 2 * PI;
+	if (player->angle > (2 * PI))
+		player->angle -= 2 * PI;
 	rotate2(player->angle, &player->vector_front);
 	perpendicular_angle = player->angle + PI / 2;
 	rotate2(perpendicular_angle, &player->vector_perpendicular);
