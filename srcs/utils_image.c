94ef35be5ad62c1a43bd7ecd831196b21a580396
Paula Biazotto <pds_paula@hotmail.com>
2024-06-07 13:35:35 +0100

    fix: changed draw player

--- srcs/utils_image.c
+++ srcs/utils_image.c
@@ -6,7 +6,7 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 10:48:15 by paula             #+#    #+#             */
-/*   Updated: 2024/06/07 13:25:10 by paula            ###   ########.fr       */
+/*   Updated: 2024/06/07 13:34:16 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,22 +73,18 @@ static void	draw_pov(t_main *cub)
 void	draw_player(t_main *cub)
 {
 	t_vector	start;
-//	t_vector	end;
 	t_vector	rigth;
 	t_vector	left;
 
 	ft_mlx_pixel_put(&cub->img, cub->player.x, cub->player.y, 0xFF0000);
 	start.dx = cub->player.x;
 	start.dy = cub->player.y;
-//	end.dx = cub->player.x - cub->player.vector_front.dx * 15;
-//	end.dy = cub->player.y - cub->player.vector_front.dy * 15;
 	rotate2(cub->player.angle + PI / 2, &rigth);
 	rigth.dx = start.dx - rigth.dx * 10;
 	rigth.dy = start.dy - rigth.dy * 10;
 	rotate2(cub->player.angle - PI / 2, &left);
 	left.dx = start.dx - left.dx * 10;
 	left.dy = start.dy - left.dy * 10;
-	//draw_line_to_frame(cub, start, end, 0xFF0000);
 	draw_line_to_frame(cub, start, rigth, 0x0000FF);
 	draw_line_to_frame(cub, start, left, 0x0000FF);
 	draw_pov(cub);
