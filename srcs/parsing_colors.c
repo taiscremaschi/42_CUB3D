5ccdb5a3a3e72f7c99400e10ced3540ab0ee2a4c
Paula Biazotto <pds_paula@hotmail.com>
2024-08-09 15:01:28 +0100

    fix :norm

--- srcs/parsing_colors.c
+++ srcs/parsing_colors.c
@@ -6,7 +6,7 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 16:02:26 by tbolzan-          #+#    #+#             */
-/*   Updated: 2024/05/29 10:17:22 by paula            ###   ########.fr       */
+/*   Updated: 2024/08/09 15:00:41 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,6 @@ void	save_color(t_main *main, int tmp, int i, char type)
 	}
 }
 
-
-
 bool	check_color_digit(char **color_content)
 {
 	int	i;
@@ -60,21 +58,20 @@ bool	check_color_digit(char **color_content)
 	return (true);
 }
 
-bool check_comma(char *color)
+bool	check_comma(char *color)
 {
-	int comma;
-	int i;
+	int	comma;
+	int	i;
 
 	comma = 0;
 	i = 0;
-
 	while (color[i])
 	{
-		if(color[i] == ',')
+		if (color[i] == ',')
 			comma++;
 		i++;
 	}
-	if(comma != 2)
+	if (comma != 2)
 		return (false);
 	return (true);
 }
@@ -84,16 +81,17 @@ bool	search_colors(t_main *main, char *color, char type)
 	char	**color_content;
 	int		i;
 	int		tmp;
+
 	if (!check_comma(color))
 		return (false);
 	color_content = ft_split(color, ',');
-	i = 0;
+	i = -1;
 	tmp = 0;
 	if (!check_args_colors(color_content))
 		return (false);
 	if (!check_color_digit(color_content))
 		return (false);
-	while (color_content[i])
+	while (color_content[++i])
 	{
 		tmp = ft_atoi(color_content[i]);
 		if (tmp < 0 || tmp > 255)
@@ -102,7 +100,6 @@ bool	search_colors(t_main *main, char *color, char type)
 			return (false);
 		}
 		save_color(main, tmp, i, type);
-		i++;
 	}
 	free_map(color_content);
 	return (true);
